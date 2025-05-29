1e3d02930ba59bf181b2e48d6e439ddfd4362dbf
Vjndevshopify <vijendra.singh@retrospec.com>
2025-05-29 10:43:32 +0530

    order links redirect

--- app/api/proxy/[...slug]/route.ts
+++ app/api/proxy/[...slug]/route.ts
@@ -15,7 +15,7 @@ export async function GET(req: NextRequest) {
       '/en_US/faq-orders':
       'https://help.retrospec.com/en-US/articles/order-and-shipping-160009',
 
-      
+
       '/en_US/order-update-information/what-should-i-do-if-i-put-the-wrong-shipping-address':
       'https://help.retrospec.com/en-US/what-should-i-do-if-i-put-the-wrong-shipping-address-603386',
 
@@ -29,6 +29,15 @@ export async function GET(req: NextRequest) {
 
       '/en_US/order-cancellation/how-do-i-cancel-my-order-if-its-already-been-shipped':
       'https://help.retrospec.com/en-US/how-do-i-cancel-my-order-if-its-already-been-shipped-603487',
+
+      'en_US/order-cancellation/can-i-cancel-my-order-if-i-havent-received-a-tracking-number-yet':
+      'https://help.retrospec.com/en-US/can-i-cancel-my-order-if-i-havent-received-a-tracking-number-yet-603488',
+       
+      '/en_US/order-cancellation/i-just-placed-my-order-and-according-to-the-tracking-number-it-hasnt-been-shipped-yet-am-i-able-to-cancel-it':
+      'https://help.retrospec.com/en-US/i-just-placed-my-order-and-according-to-the-tracking-number-it-hasnt-been-shipped-yet-am-i-able-to-cancel-it-651587',
+       
+      '/en_US/order-general/the-product-i-want-is-out-of-stock-what-do-i-do':
+      'https://help.retrospec.com/en-US/the-product-i-want-is-out-of-stock-what-do-i-do-651588',
 
 
   };
