6b81244c245e95368bab7ead954482d8a5069d5e
Vjndevshopify <vijendra.singh@retrospec.com>
2025-06-05 11:04:25 +0530

    url redire fix

--- app/api/proxy/[...slug]/route.ts
+++ app/api/proxy/[...slug]/route.ts
@@ -65,7 +65,7 @@ export async function GET(req: NextRequest) {
   '/en_US/returns-/how-do-i-start-a-return': 'https://help.retrospec.com/en-US/how-do-i-start-a-return-651595',
   '/en_US/exchanges/do-you-offer-exchanges': 'https://help.retrospec.com/en-US/do-you-offer-exchanges-651597',
   '/en_US/exchanges/my-bike-isnt-the-right-size-can-i-exchange-it-for-a-new-one': 'https://help.retrospec.com/en-US/my-bike-isnt-the-right-size-can-i-exchange-it-for-a-new-one-651598',
-  'en_US/damaged-product/my-order-arrived-damaged-what-do-i-do': 'https://help.retrospec.com/en-US/my-order-arrived-damaged-what-do-i-do-1724433',
+  '/en_US/damaged-product/my-order-arrived-damaged-what-do-i-do': 'https://help.retrospec.com/en-US/my-order-arrived-damaged-what-do-i-do-1724433',
 
   
   '/en_US/faq-general': 'https://help.retrospec.com/en-US/articles/company-and-partners-160494',
