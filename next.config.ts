434456a65f68656cebeb01e61cc0d2e75ee4e760
Vjndevshopify <vijendra.singh@retrospec.com>
2025-06-05 11:55:23 +0530

    url redire fix

--- next.config.ts
+++ next.config.ts
@@ -13,21 +13,7 @@ const nextConfig = {
     ],
   },
 
-  async redirects() {
-    return [
-      {
-        source: '/en_US/bike_general/what\'s-the-difference-between-a-step-over-and-step-through-bike-frame',
-        destination: 'https://help.retrospec.com/en-US/articles/your-article-slug',
-        permanent: true,
-      },
-      {
-        // fallback for encoded curly apostrophe (%E2%80%99)
-        source: '/en_US/bike_general/what’s-the-difference-between-a-step-over-and-step-through-bike-frame',
-        destination: 'https://help.retrospec.com/en-US/articles/your-article-slug',
-        permanent: true,
-      },
-    ];
-  },
+  
 
 };
 
